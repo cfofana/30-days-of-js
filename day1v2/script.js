6fd7530d039c37040fca34c126db9a03d81df48f
Ceesay Fofana <22731598+cfofana@users.noreply.github.com>
2023-10-07 22:19:57 -0400

    more practice

--- day1v2/script.js
+++ day1v2/script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// let message = "hello";
+// message = 12353;
+// // template literals
+// const userFirstName = "jack";
+// const userPromptedName = prompt("what is your name?", userFirstName);
+// message = `Your name is ${userPromptedName}`;
+// const greetingHello = "hello";
+// const greetingHow = "how are you?";
+// console.log(`${greetingHello}, ${greetingHow}`); 
+
+// const btn = document.querySelector(".btn");
+// const greeting = document.querySelector(".greeting");
+
+// function greet(){
+//     let userName = prompt("What is your name?");
+//     let msg = `Hello ${userName}, how are you doing?`;
+//     greeting.textContent = msg;
+// }
+
+// btn.addEventListener("click", greet);
+const song = "Is you is, or is you ain't my baby?";
+const msg = `I like the song '${song}'. I give it a score of ${(9/10)*100}%`;
+console.log(msg);
